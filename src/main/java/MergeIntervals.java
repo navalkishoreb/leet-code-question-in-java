e1ba7e9d1a676840c27de79fe7f2dabd3e60c6e1
Naval Kishore Barthwal <navalkishoreb@gmail.com>
2025-12-26 16:58:29 +0530

    56. MergeIntervals

---
+++ src/main/java/MergeIntervals.java
@@ -0,0 +1,69 @@
+/**
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ * Example 2:
+ * <p>
+ * Input: intervals = [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ * Example 3:
+ * <p>
+ * Input: intervals = [[4,7],[1,4]]
+ * Output: [[1,7]]
+ * Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ */
+
+import java.util.*;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] testCase1 = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] testCase2 = {{1, 4}, {4, 5}};
+        int[][] testCase3 = {{4, 7}, {1, 4}};
+        System.out.println(Arrays.deepToString(solution.merge(testCase1)));
+        System.out.println(Arrays.deepToString(solution.merge(testCase2)));
+        System.out.println(Arrays.deepToString(solution.merge(testCase3)));
+    }
+
+
+}
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> result = new ArrayList<>();
+        if (intervals == null || intervals.length == 0) {
+            return new int[][]{};
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                result.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        result.add(new int[]{start, end});
+        return result.toArray(new int[result.size()][]);
+
+    }
+}
