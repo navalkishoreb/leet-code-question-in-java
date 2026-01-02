91303c387943b559af8cb3ddd0dfce65ab061e51
Naval Barthwal <navalkishoreb@gmail.com>
2026-01-02 14:00:56 +0530

    quest array II question 3

---
+++ src/main/java/quest/arrayII/FindAllNumberDisapperedInAnArray.java
@@ -0,0 +1,66 @@
+package quest.arrayII;
+
+/**
+ * Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
+ * Example 2:
+ *
+ * Input: nums = [1,1]
+ * Output: [2]
+ *
+ *
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 105
+ * 1 <= nums[i] <= n
+ *
+ *
+ * Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ */
+public class FindAllNumberDisapperedInAnArray {
+
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            int[] data = new int[nums.length+1];
+            List<Integer> res = new ArrayList<>();
+
+            for(int i = 0; i < nums.length; i++){
+                data[nums[i]]++;
+            }
+
+            for(int i = 1 ; i < nums.length + 1; i++){
+                if(data[i] == 0){
+                    res.add(i);
+                }
+            }
+            return res;
+        }
+    }
+    class Solution2 {
+        // without extra space
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            List<Integer> res = new ArrayList<>();
+
+            for(int i = 0; i < nums.length; i++){
+                int index = Math.abs(nums[i]) -1;
+                if (nums[index] > 0){
+                    nums[index] = -nums[index];
+                }
+            }
+
+            for(int i = 0 ; i < nums.length; i++){
+                if(nums[i] > 0){
+                    res.add(i+1);
+                }
+            }
+            return res;
+        }
+    }
+}
