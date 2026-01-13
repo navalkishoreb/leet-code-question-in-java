387bb1d77eae081ee0a7a105be92379bf5e14dac
Naval Barthwal <navalkishoreb@gmail.com>
2026-01-13 17:25:11 +0530

    linkedlist

--- src/main/java/quest/linkedlist/OddEvenLinkedList.java
+++ src/main/java/quest/linkedlist/OddEvenLinkedList.java
@@ -39,43 +39,15 @@ public class OddEvenLinkedList {
      * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
     class Solution {
         public ListNode oddEvenList(ListNode head) {
-            if (head == null){
+            if (head == null) {
                 return head;
             }
             ListNode odd = head;
             ListNode even = head.next;
             ListNode evenHead = even;
-            while(even !=null && even.next !=null){
+            while (even != null && even.next != null) {
                 odd.next = even.next;
                 odd = odd.next;
                 even.next = odd.next;
