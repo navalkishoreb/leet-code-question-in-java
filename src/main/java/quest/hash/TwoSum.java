a7268a9feecaa478b81fc2bc04e97a3499648976
Naval Barthwal <navalkishoreb@gmail.com>
2026-01-13 18:31:26 +0530

    hashmap

--- src/main/java/quest/hash/TwoSum.java
+++ src/main/java/quest/hash/TwoSum.java
@@ -41,16 +41,13 @@ public class TwoSum {
     class Solution {
         public int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                map.put(nums[i], i);
-            }
-            for (int i = 0; i < nums.length; i++) {
-                int second = target - nums[i];
-                Integer index = map.get(second);
-                if (index != null && index != i) {
-                    return new int[]{i, map.get(second)};
-                }
-            }
+           for(int i = 0; i < nums.length; i++){
+               int complement = target - nums[i];
+               if(map.containsKey(complement)){
+                   return new int[]{map.get(complement), i};
+               }
+               map.put(nums[i], i);
+           }
             return new int[]{};
         }
     }
