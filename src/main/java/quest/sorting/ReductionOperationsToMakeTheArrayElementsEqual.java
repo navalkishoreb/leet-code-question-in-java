8de76fb499c5b59549b2fb3a9126544f2abd97b2
Naval Barthwal <navalkishoreb@gmail.com>
2026-01-29 14:21:38 +0530

    quest sorting reduce to make array element equal tree map + level

--- src/main/java/quest/sorting/ReductionOperationsToMakeTheArrayElementsEqual.java
+++ src/main/java/quest/sorting/ReductionOperationsToMakeTheArrayElementsEqual.java
@@ -6,7 +6,8 @@ import java.util.TreeMap;
 /**
  * Given an integer array nums, your goal is to make all elements in nums equal. To complete one operation, follow these steps:
  * <p>
- * Find the largest value in nums. Let its index be i (0-indexed) and its value be largest. If there are multiple elements with the largest value, pick the smallest i.
+ * Find the largest value in nums. Let its index be i (0-indexed) and its value be largest.
+ * If there are multiple elements with the largest value, pick the smallest i.
  * Find the next largest value in nums strictly smaller than largest. Let its value be nextLargest.
  * Reduce nums[i] to nextLargest.
  * Return the number of operations to make all elements in nums equal.
