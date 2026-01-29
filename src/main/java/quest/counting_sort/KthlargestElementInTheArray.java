aba46ce97f181e72277480fac3098a39cc25bcf0
Naval Barthwal <navalkishoreb@gmail.com>
2026-01-29 14:19:43 +0530

    counting sort kth largest element heap sort

---
+++ src/main/java/quest/counting_sort/KthlargestElementInTheArray.java
@@ -0,0 +1,46 @@
+package quest.counting_sort;
+
+import java.util.PriorityQueue;
+
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+
+Constraints:
+
+1 <= k <= nums.length <= 10^5
+-104 <= nums[i] <= 10^4
+ */
+public class KthlargestElementInTheArray {
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            for (int n : nums) {
+                minHeap.offer(n);
+                if (minHeap.size() > k) {
+                    minHeap.poll();
+                }
+            }
+            return minHeap.peek();
+
+        }
+    }
+}
