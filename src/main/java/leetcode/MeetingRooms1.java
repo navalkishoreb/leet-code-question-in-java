e14c5597005af2ec284281329223733197fc60ca
Naval Kishore Barthwal <navalkishoreb@gmail.com>
2026-02-03 11:53:13 +0530

    leet code amend description

--- src/main/java/leetcode/MeetingRooms1.java
+++ src/main/java/leetcode/MeetingRooms1.java
@@ -4,7 +4,8 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+ * Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i),
+ * determine if a person could add all meetings to their schedule without any conflicts.
  * <p>
  * Note: (0,8),(8,10) is not considered a conflict at 8
  * <p>
