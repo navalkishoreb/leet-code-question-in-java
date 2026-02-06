1c50d679c2cd5629284694d76e7323b14378c338
Naval Kishore Barthwal <navalkishoreb@gmail.com>
2026-02-06 11:41:59 +0530

    leetcode quest prefix sum refactor

--- src/main/java/quest/prefix_sum/MakeSumDivisibleByP.java
+++ src/main/java/quest/prefix_sum/MakeSumDivisibleByP.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Given an array of positive integers nums, remove the smallest subarray (possibly empty) such that the sum of the remaining elements is divisible by p. It is not allowed to remove the whole array.
+ * Given an array of positive integers nums, remove the smallest subarray (possibly empty) such that the sum of the remaining elements is divisible by p.
+ * It is not allowed to remove the whole array.
  * <p>
  * Return the length of the smallest subarray that you need to remove, or -1 if it's impossible.
  * <p>
@@ -16,12 +17,14 @@ import java.util.Map;
  * <p>
  * Input: nums = [3,1,4,2], p = 6
  * Output: 1
- * Explanation: The sum of the elements in nums is 10, which is not divisible by 6. We can remove the subarray [4], and the sum of the remaining elements is 6, which is divisible by 6.
+ * Explanation: The sum of the elements in nums is 10, which is not divisible by 6.
+ * We can remove the subarray [4], and the sum of the remaining elements is 6, which is divisible by 6.
  * Example 2:
  * <p>
  * Input: nums = [6,3,5,2], p = 9
  * Output: 2
- * Explanation: We cannot remove a single element to get a sum divisible by 9. The best way is to remove the subarray [5,2], leaving us with [6,3] with sum 9.
+ * Explanation: We cannot remove a single element to get a sum divisible by 9.
+ * The best way is to remove the subarray [5,2], leaving us with [6,3] with sum 9.
  * Example 3:
  * <p>
  * Input: nums = [1,2,3], p = 3
--- src/main/java/quest/prefix_sum/WayToMakeFairArray.java
+++ src/main/java/quest/prefix_sum/WayToMakeFairArray.java
@@ -1,7 +1,9 @@
 package quest.prefix_sum;
 
 /**
- * You are given an integer array nums. You can choose exactly one index (0-indexed) and remove the element. Notice that the index of the elements may change after the removal.
+ * You are given an integer array nums.
+ * You can choose exactly one index (0-indexed) and remove the element.
+ * Notice that the index of the elements may change after the removal.
  * <p>
  * For example, if nums = [6,1,7,4,1]:
  * <p>
